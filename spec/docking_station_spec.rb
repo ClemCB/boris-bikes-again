0af0404c4360ec9f3fd74e307610b2104e1dd6cb
Nicholas Leacock <nicholas@swissmail.com>
2017-02-15 15:21:16 +0000

    Update to Boris Bike program and spec.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,15 @@
 require 'docking_station'
 
+
 describe DockingStation do
 
   it "responds to release_bike" do
     expect(subject).to respond_to :release_bike
   end
+
+  it "returns a new instance of Bike.new" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
 end
